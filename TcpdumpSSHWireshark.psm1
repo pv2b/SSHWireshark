8d1d31c3635f088adb5f8242b9bb25a5772b5eb3
Per von Zweigbergk <pvz@pvz.pp.se>
2020-07-06 13:24:10 +0200

    Add immediate mode to tcpdump

--- TcpdumpSSHWireshark.psm1
+++ TcpdumpSSHWireshark.psm1
@@ -16,11 +16,19 @@ function Invoke-TcpdumpSSHWireshark {
     
     Process {
         # TODO Add escaping...
+
+        # --immediate-mode
+        #      Capture  in  "immediate mode".  In this mode, packets are deliv-
+        #      ered to tcpdump as  soon  as  they  arrive,  rather  than  being
+        #      buffered  for  efficiency.   This  is  the default when printing
+        #      packets rather than saving packets  to  a  ``savefile''  if  the
+        #      packets are being printed to a terminal rather than to a file or
+        #      pipe.
         # -U for unbuffered output from tcpdump
         # -n to disable name resolution
         # -i to choose interface
         # -w - to write to stdout
-        $Command = "tcpdump -Un -i $Interface -w - $Expression"
+        $Command = "tcpdump --immediate-mode -Un -i $Interface -w - $Expression"
 
         $CommonSSHParameters = _GetCommonSSHParameters -CallerPSBoundParameters $PSBoundParameters
         Invoke-SSHWireshark -Command $Command @CommonSSHParameters
